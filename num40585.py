ac11ee838f59dae8a8e497030275e08dbe50cf1f
hadesky <582990537@qq.com>
2021-07-09 17:46:15 +0800

    测试5

---
+++ num40585.py
@@ -0,0 +1,16 @@
+from math import factorial
+'有哪些数字，它每个位数的阶乘相加等于它本身？'
+
+
+def func(x):
+    result = 0
+    while x > 0:
+        result += factorial(x % 10)
+        x //= 10
+    return result
+
+
+num = 0
+for i in range(1, 9999999):
+    if i == func(i):
+        print(i)
